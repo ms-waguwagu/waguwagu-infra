e2e6b0bd8f9f0243dfe1fa48c97d945c81923454
Zuzzang2 <mandy021021@gmail.com>
2026-01-02 14:29:47 +0900

    chore: dev관련 정리 및 레플리카 수정

--- terraform/game/main.tf
+++ terraform/game/main.tf
@@ -40,7 +40,7 @@ module "eks" {
   vpc_id            = data.aws_vpc.game.id
   subnet_ids      = [data.aws_subnet.game_private_a.id, data.aws_subnet.game_private_b.id]
   node_group_name = "game-node-group-v2"
-  instance_types    = ["t3.medium"]
+  instance_types    = ["t3.large"]
   disk_size       = 60
   desired_size      = 4
   max_size          = 5
@@ -50,9 +50,9 @@ module "eks" {
   create_public_node_group = true
   public_node_group_name   = "game-public-node-group"
   public_subnet_ids        = [data.aws_subnet.game_public_a.id, data.aws_subnet.game_public_b.id]
-  public_instance_types    = ["t3.medium"]
-  public_desired_size      = 3
-  public_max_size          = 4
+  public_instance_types    = ["t3.large"]
+  public_desired_size      = 4
+  public_max_size          = 5
   public_min_size          = 1
 }
 
